da97154ea48d9ca9d65b012e2ccab466843e9d11
K. Sabbak & Simon Swartzman <pair+ksabbak.rtzm@devbootcamp.com>
2017-04-27 16:22:33 -0500

    Fix errors, test views, create post route logic

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -6,12 +6,18 @@ end
 
 post '/questions' do
   # TODO: check_login
-
-
+  question = Question.new(params[:question])
+  # TODO: question.author = current_user
+  question.author = User.first
+  if question.save
+    redirect "/questions/#{question.id}"
+  else
+    @errors = question.errors.full_messages
+    erb :"questions/new"
+  end
 end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
-
   erb :'questions/show'
 end
