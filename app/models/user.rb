906a33750ca0d6bebc2feb21fd5f185299ee95f2
Joe LeBeau <email": null>
2017-04-27 12:35:40 -0500

    add non-polymorphic associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :questions, foreign_key: :author_id
+  has_many :answers, foreign_key: :responder_id
+  has_many :comments, foreign_key: :commenter_id
+  has_many :votes, foreign_key: :voter_id
 end
