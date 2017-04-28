fc3194fcc20462cad8bdbfc728af2b1f11b5b3ce
Stacy Curry & Tove N Mercer <pair+stacurry.tovemercer@devbootcamp.com>
2017-04-28 12:15:12 -0500

    add form for creating new answer; post route logic; login validation

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,6 +1,11 @@
 post '/questions/:id/answers' do
+  check_login
   @question = Question.find(params[:id])
-  # @answer = Answer.new(body: , responder_id: session[:user_id])
-  @question.answers << @answer
-  redirect "/questions/:id"
+  @answer = Answer.new(body: params[:body], responder_id: session[:user_id])
+  if @question.answers << @answer
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @answer.errors.full_messages
+    erb :"questions/show"
+  end
 end
