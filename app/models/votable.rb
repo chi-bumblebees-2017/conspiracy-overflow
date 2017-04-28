67fb33864f9cceed742d8fe186104d6f1f12cef8
K. Sabbak & Simon Swartzman <pair+ksabbak.rtzm@devbootcamp.com>
2017-04-28 16:30:50 -0500

    Fix bug in old vote destruction and new vote creation

--- app/models/votable.rb
+++ app/models/votable.rb
@@ -4,35 +4,38 @@ module Votable
   end
 
   def upvote_from(user)
-    if ( vote = vote_by(user) )
-      Vote.destroy(vote.id) if vote.value == -1
-    end
+    destroy_vote_if_value(user, -1)
     create_vote(user, 1) unless vote_by(user)
   end
 
   def downvote_from(user)
-    if ( vote = vote_by(user) )
-      Vote.destroy(vote.id) if vote.value == 1
-    end
+    destroy_vote_if_value(user, 1)
     create_vote(user, -1) unless vote_by(user)
-
   end
 
   def vote_by(user)
-    old_vote = votes.find do |vote|
+    votes.find do |vote|
       vote.voter.id == user.try(:id)
     end
   end
 
   def vote_match(direction, user)
-    if direction == "up"
-      vote_by(user).try(:value) == 1
-    else
-      vote_by(user).try(:value) == -1
+    value_to_check = (direction == "up" ) ? 1 : -1
+    vote_by(user).try(:value) == value_to_check
+  end
+
+  private
+
+  def destroy_vote_if_value(user, value)
+    if ( vote = vote_by(user) )
+      if vote.value == value
+        Vote.destroy(vote.id)
+        # this refreshes the object's associations so that the destroy updates its votes collection
+        reload
+      end
     end
   end
 
-  # private
   def create_vote(user, value)
     new_vote = Vote.new(voter: user, value: value)
     votes << new_vote
