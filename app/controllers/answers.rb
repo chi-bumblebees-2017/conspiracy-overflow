206ff2bcf743658dd631008005075ef68359f321
Stacy Curry & Tove N Mercer <pair+stacurry.tovemercer@devbootcamp.com>
2017-04-28 18:34:16 -0500

    ajaxifying add answer form

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -2,6 +2,7 @@ post '/questions/:id/answers' do
   check_login
   @question = Question.find(params[:id])
   @answer = Answer.new(body: params[:body], responder_id: session[:user_id])
+
   if @question.answers << @answer
     redirect "/questions/#{@question.id}"
   else
@@ -11,11 +12,13 @@ post '/questions/:id/answers' do
 end
 
 put '/questions/:question_id/answers/:id' do
+  check_login
   @question = Question.find(params[:question_id])
   @answer = Answer.find(params[:id])
 
-  @question.clear_favorites
-  @answer.set_favorited
-
+  if user_is_author?(@question.author)
+    @question.clear_favorites
+    @answer.set_favorited
+  end
   redirect "/questions/#{@question.id}"
 end
