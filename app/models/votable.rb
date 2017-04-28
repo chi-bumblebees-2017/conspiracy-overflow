d146197224f47e56b0191fc1d835ba830cadfe6a
K. Sabbak & Simon Swartzman <pair+ksabbak.rtzm@devbootcamp.com>
2017-04-28 15:53:42 -0500

    Begin redesign of vote logic for changing votes

--- app/models/votable.rb
+++ app/models/votable.rb
@@ -4,20 +4,40 @@ module Votable
   end
 
   def upvote_from(user)
-    unless voted_on_by?(user)
-      votes << Vote.new(voter: user, value: 1)
+    if ( vote = vote_by(user) )
+      Vote.destroy(vote.id) if vote.value == -1
     end
+    create_vote(user, 1) unless vote_by(user)
   end
 
   def downvote_from(user)
-    unless voted_on_by?(user)
-      votes << Vote.new(voter: user, value: -1)
+    if ( vote = vote_by(user) )
+      Vote.destroy(vote.id) if vote.value == 1
+    end
+    create_vote(user, -1) unless vote_by(user)
+
+  end
+
+  def vote_by(user)
+    old_vote = votes.find do |vote|
+      vote.voter.id == user.try(:id)
     end
   end
 
-  def voted_on_by?(user)
-    votes.any? do |vote|
-      vote.voter.id == user.id
+  def vote_match(direction, user)
+    if direction == "up"
+      vote_by(user).try(:value) == 1
+    else
+      vote_by(user).try(:value) == -1
     end
   end
+
+  # private
+  def create_vote(user, value)
+    new_vote = Vote.new(voter: user, value: value)
+    votes << new_vote
+    new_vote
+  end
+
 end
+
