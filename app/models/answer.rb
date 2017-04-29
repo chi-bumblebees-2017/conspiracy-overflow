53b4ce8d53738ad21eb4bb4930a9879e8029d5d9
K. Sabbak & Simon Swartzman <pair+ksabbak.rtzm@devbootcamp.com>
2017-04-29 16:07:53 -0500

    Add secret validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,14 @@
 class Answer < ActiveRecord::Base
   include Votable
+  include Secrets
+
   belongs_to :responder, class_name: 'User'
   belongs_to :question
   has_many :votes, as: :votable
   has_many :comments, as: :commentable
   validates :body, presence: true
+
+  validate :secrets
 
   def set_favorited
     self.favorited = true
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,12 @@
 class Comment < ActiveRecord::Base
   include Votable
+  include Secrets
+
   has_many :votes, as: :votable
   belongs_to :commentable, polymorphic: true
   belongs_to :commenter, class_name: 'User'
+
+  validate :secrets
 
   validates_presence_of :commenter_id, :commentable_id, :commentable_type, :body
 end
